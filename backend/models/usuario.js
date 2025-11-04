87cdd6fe9e77a8fbda33fb395244d840e7fce00c
emilio <emiliocastillos1406@gmail.com>
2025-11-04 02:06:56 -0300

    backendpt1

---
+++ backend/models/usuario.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const usuarioSchema = new mongoose.Schema({
+    nombre:String,
+    password:String
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
